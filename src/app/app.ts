0dadef91012452dc80f21b4140c70172dccd2dbd
Andy Igiraneza <w0876175@myscc.ca>
2025-10-07 11:01:23 -0400

    show single ContentListItem above list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,23 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {Product} from './models/product';
 import { ProductList } from './product-list/product-list';
+import {ProductService} from './services/product.service';
+import {ProductListItem} from './product-list-item/product-list-item';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductList],
+  imports: [ProductList, ProductListItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  title: string = 'Assignment 3';
+export class App implements OnInit {
+  title: string = 'Assignment 4';
+  featured?: Product;
+
+  constructor(private productSvc: ProductService) {}
+
+  ngOnInit(): void {
+    // pick any existing id (e.g., 2)
+    this.productSvc.read(2).subscribe(item => (this.featured = item));
+  }
 }
