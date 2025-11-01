36a2e8fb4ff100501891b2d86e90d612c778ea1f
Andy Igiraneza <w0876175@myscc.ca>
2025-10-31 22:29:42 -0400

    Reactive Form Input and CRUD Verification

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,12 @@ import { ProductList } from './product-list/product-list';
 import {ProductService} from './services/product.service';
 import {ProductListItem} from './product-list-item/product-list-item';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [ProductList, ProductListItem, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [ProductList, ProductListItem, RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
